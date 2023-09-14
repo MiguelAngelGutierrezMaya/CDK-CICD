e5a86f6e9d27cadba0b1768897943def58ee3f02
Miguel Angel <miguel.gutierrez@kbe.ai>
2023-09-13 23:16:20 -0500

    cicd-practice | Adding test stages

--- test/cdk-cicd.test.ts
+++ test/cdk-cicd.test.ts
@@ -1,3 +1,16 @@
-test('Initial test', () => {
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from 'aws-lambda';
+import { handler } from '../services/hello';
 
+describe('Hello describe test cdk-cicd', () => {
+  test('Hello test cdk-cicd', async () => {
+    const result: APIGatewayProxyResult = await handler(
+      {} as APIGatewayProxyEvent,
+      {} as Context,
+    );
+    expect(result.statusCode).toBe(200);
+  });
 });
